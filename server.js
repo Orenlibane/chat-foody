9941b89da05c995821961071ca9575f2b886862e
Tzezanas <orenlibane@gmail.com>
2021-07-18 13:37:58 +0300

    test2

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "1476955067";
+    let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
     // Parse the query params
     let mode = req.query['hub.mode'];
